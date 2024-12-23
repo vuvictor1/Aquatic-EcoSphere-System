a7aec51b7c23c0225b0043b6a2378a7c24af283b
jmor1591 <bluejm01@gmail.com>
2024-12-23 12:19:46 -0800

    updated databse.py to use new .env

--- Dash_playground/database.py
+++ Dash_playground/database.py
@@ -1,17 +1,33 @@
 # database.py
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from urllib.parse import urlparse  # Import urlparse
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 def get_database_connection():
+    # Parse database URL from .env file
+    db_url = os.getenv('DATABASE_URL')
+    parsed_url = urlparse(db_url)
+
     # Database configuration
     db_config = {
-        'user': 'root',
-        'password': 'ZAJDlxblTEhBCDhOsvxwwQDXjWWCfPoR',
-        'host': 'autorack.proxy.rlwy.net',
-        'port': 22542,
-        'database': 'railway'
+        'user': parsed_url.username,
+        'password': parsed_url.password,
+        'host': parsed_url.hostname,
+        'port': parsed_url.port,
+        'database': parsed_url.path[1:]  # Remove leading '/'
     }
+
+    # Check for missing environment variables
+    for key, value in db_config.items():
+        if value is None:
+            raise ValueError(f"Missing environment variable: {key}")
+
     # Create the connection string
     connection_string = f"mysql+mysqlconnector://{db_config['user']}:{
         db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
