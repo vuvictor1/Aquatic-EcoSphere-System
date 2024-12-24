dfe15fd2268ceb51ba9c317f50e7d807664d05d1
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2024-12-23 16:05:21 -0800

    cleaned file

--- db_connection.py
+++ db_connection.py
@@ -1,24 +1,25 @@
-# db_connection.py
+# Author: Jordan Morris
+# File: connect_timer.py
+# Description: Adds a connection to MySQL for all local server files
+#Copyright (C) 2024 Victor V. Vu and Jordan Morris
+#License: GNU GPL v3 - See https://www.gnu.org/licenses/gpl-3.0.en.html
 import pymysql
 from dotenv import load_dotenv
 import os
 from urllib.parse import urlparse
 
-load_dotenv()  # Load environment variables from .env
+load_dotenv() # Load environment variables from .env
 
 # Parse database URL from .env file
 db_url = os.getenv('DATABASE_URL')
 parsed_url = urlparse(db_url)
 
-# Establish MySQL connection
-
-
-def create_connection():
+def create_connection(): # Establish MySQL connection
     return pymysql.connect(
         host=parsed_url.hostname,
         user=parsed_url.username,
         password=parsed_url.password,
         database=parsed_url.path[1:],
         port=parsed_url.port,
-        autocommit=True  # enable autocommit to refresh data
+        autocommit=True # autocommit to refresh data
     )
