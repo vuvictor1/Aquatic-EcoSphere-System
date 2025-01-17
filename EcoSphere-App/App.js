5ff8cd3716143de267a103a3da6df67b94f9410f
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-16 20:27:21 -0800

    simplified app and added icon

---
+++ EcoSphere-App/App.js
@@ -0,0 +1,35 @@
+// Authors: Victor Vu and Jordan Morris
+// File: app.js
+// Description: Mobile app for Aquatic EcoSphere web app.
+// Copyright (C) 2025 Victor V. Vu and Jordan Morris
+// License: GNU GPL v3 - See https://www.gnu.org/licenses/gpl-3.0.en.html
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => { // App component
+  return ( 
+    <SafeAreaView style={styles.container}> 
+      <WebView // WebView component to display the web app
+        source={{ uri: 'https://aquatic-eco.up.railway.app/' }} // use web app URL
+        style={styles.webview}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUser Action={false}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({ // Set styles
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default App; // export App component
