a43df10bb4757e2e3e02c82f1de68a45a73d5f67
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-19 16:00:27 -0800

    reminder placeholder

--- web_functions.py
+++ web_functions.py
@@ -5,26 +5,19 @@
 # License: GNU GPL v3 - See https://www.gnu.org/licenses/gpl-3.0.en.html
 from nicegui import ui
 
+def eco_header(): # Header for the web interface
+    with ui.header().style('background-color: #3AAFA9; padding: 10px 20px; position: static;'): 
+        ui.link('🌊 Home', '/').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
+        ui.link('Reminders', '/reminders').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
+        ui.link('Graphs', '/graphs').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
+        ui.link('Encyclopedia', '/encyclopedia').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
+        ui.link('Contacts', '/contacts').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
 
-def eco_header():  # Header for the web interface
-    with ui.header().style('background-color: #3AAFA9; padding: 10px 20px; position: static;'):  # Header style
-        ui.link(
-            '🌊 Home', '/').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
-        ui.link(
-            'Graphs', '/graphs').style('color: #FFFFFF; font-size: 24px; text-decoration: none;')
-        ui.link('Encyclopedia', '/encyclopedia').style(
-            'color: #FFFFFF; font-size: 24px; text-decoration: none;')
-        ui.link('Contacts', '/contacts').style(
-            'color: #FFFFFF; font-size: 24px; text-decoration: none;')
+        with ui.row().style('gap: 10px;'): # Buttons style
+            ui.button('account', icon='account_circle', on_click=lambda: ui.navigate.to('/login')) # account redirect
+            ui.button(icon='settings', on_click=lambda: ui.navigate.to('/settings')) # settings redirect
 
-        with ui.row().style('gap: 10px;'):  # Buttons style
-            ui.button('account', icon='account_circle', on_click=lambda: ui.navigate.to(
-                '/login'))  # account redirect
-            ui.button(icon='settings', on_click=lambda: ui.navigate.to(
-                '/settings'))  # settings redirect
-
-
-def inject_style():  # Injects CSS style in web interface
+def inject_style(): # Injects CSS style in web interface
     ui.add_head_html("""
     <style>
         body {
@@ -65,13 +58,9 @@ def inject_style():  # Injects CSS style in web interface
     </style>
     """)
 
+def inject_lottie(): # Injects Lottie animation in web interface
+    ui.add_body_html('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
 
-def inject_lottie():  # Injects Lottie animation in web interface
-    ui.add_body_html(
-        '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>')
-
-
-def eco_footer():  # Footer for the web interface
+def eco_footer(): # Footer for the web interface
     with ui.footer().style('background-color: #3AAFA9; justify-content: center; padding: 10px; position: static;'):
-        ui.label('Copyright (C) 2025 | Victor Vu & Jordan Morris').style(
-            'color: #FFFFFF; font-size: 18px;')
+        ui.label('Copyright (C) 2025 | Victor Vu & Jordan Morris').style('color: #FFFFFF; font-size: 18px;')
