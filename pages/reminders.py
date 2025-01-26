dfd93ad2e0496b93c604b94c7fc40756a7213a3d
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-26 14:23:46 -0800

    connected reminders to dashboard

--- pages/reminders.py
+++ pages/reminders.py
@@ -4,14 +4,15 @@
 # Copyright (C) 2025 Victor V. Vu and Jordan Morris
 # License: GNU GPL v3 - See https://www.gnu.org/licenses/gpl-3.0.en.html
 
-# Note: Current implementation is of reminder data is not persistent and is missing notification.-------------------
+# Note: Current implementation is of reminder data is not persistent, is missing notification and cannot update on the dashboard.
 import time
 from nicegui import ui
 from web_functions import inject_style, eco_header, eco_footer
 
+upcoming_task = None # global variable for the upcoming task
 def get_upcoming_task(rows): # Task with the least amount of days for dashboard
-
     filtered_rows = [row for row in rows if int(row['frequency']) != 99] # ignore the example task
+
     if not filtered_rows: # If no tasks
         return None
     return min(filtered_rows, key=lambda x: int(x['frequency'])) 
@@ -53,14 +54,10 @@ def reminders_page(): # Renders the reminders page
         ui.button('Remove', on_click=lambda: (table.remove_rows(table.selected), update_task(rows))) # take out items 
     eco_footer() # add footer
 
-    def update_task(rows): # Function to update the label with the task with the least amount of days left --------send tto the dashbaord later
+    def update_task(rows): # Function to update the label with the task with the least amount of days left
+        global upcoming_task 
         least_days_task = get_upcoming_task(rows) # store the task with the least amount of days
-        if least_days_task: # If there is a task
-            least_days_label.set_text(f"Task with the least days left: {least_days_task['task']} ({least_days_task['frequency']} days)")
-        else: # If there is no task
-            least_days_label.set_text("You have no urgent tasks")
-
-    least_days_label = ui.label() # initialize the label
+        upcoming_task = least_days_task # update the global variable
     update_task(rows) # update the label
 
 @ui.page('/reminders') # Route for reminders page
