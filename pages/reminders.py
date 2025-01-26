1f0808dc3aa66c2a394b770c023cd259ddfa0f3e
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-26 13:12:03 -0800

    code refactor

--- pages/reminders.py
+++ pages/reminders.py
@@ -4,17 +4,18 @@
 # Copyright (C) 2025 Victor V. Vu and Jordan Morris
 # License: GNU GPL v3 - See https://www.gnu.org/licenses/gpl-3.0.en.html
 
-# Note: Current implementation is of reminder data is not persistent.-------------------
+# Note: Current implementation is of reminder data is not persistent and is missing notification.-------------------
 import time
 from nicegui import ui
 from web_functions import inject_style, eco_header, eco_footer
 
-def get_task_with_least_days(rows): # Task with the least amount of days for dashboard
+def get_upcoming_task(rows): # Task with the least amount of days for dashboard
+
     filtered_rows = [row for row in rows if int(row['frequency']) != 99] # ignore the example task
     if not filtered_rows: # If no tasks
         return None
     return min(filtered_rows, key=lambda x: int(x['frequency'])) 
-
+ 
 def reminders_page(): # Renders the reminders page
     eco_header()
     inject_style()
@@ -29,7 +30,7 @@ def reminders_page(): # Renders the reminders page
 
     with ui.column().style('align-items: center; width: 100%;'): # Center the column
         # Create the table
-        with ui.table(title='Aquarium Maintenance Tasks', columns=columns, rows=rows, selection='multiple', pagination=10).classes('w-96') as table:
+        with ui.table(title='Maintenance Tasks', columns=columns, rows=rows, selection='multiple', pagination=10).classes('w-96') as table:
             with table.add_slot('top-right'): # Add a search bar to search for task
                 with ui.input(placeholder='Search').props('type=search').bind_value(table, 'filter').add_slot('append'):
                     ui.icon('search') 
@@ -41,31 +42,26 @@ def reminders_page(): # Renders the reminders page
                             table.add_row({'id': time.time(), 'task': new_task.value, 'frequency': new_frequency.value}), 
                             new_task.set_value(None), # default value none
                             new_frequency.set_value(None),
-                            update_least_days_label(rows) # update the least days label
+                            update_task(rows) # update the least days label
                         ), icon='add').props('flat fab-mini') 
                         
                     with table.cell(): # Input for new task
                         new_task = ui.input('Task')
+
                     with table.cell(): # Input for new frequency
                         new_frequency = ui.input('Frequency')
-
-        ui.button('Remove', on_click=lambda: (
-            table.remove_rows(table.selected),
-            update_least_days_label(rows) # Update the least days label
-        )) # take out items 
+        ui.button('Remove', on_click=lambda: (table.remove_rows(table.selected), update_task(rows))) # take out items 
     eco_footer() # add footer
 
-    # Function to update the label with the task with the least amount of days left --------send tto the dashbaord later
-    def update_least_days_label(rows):
-        least_days_task = get_task_with_least_days(rows)
-        if least_days_task:
+    def update_task(rows): # Function to update the label with the task with the least amount of days left --------send tto the dashbaord later
+        least_days_task = get_upcoming_task(rows) # store the task with the least amount of days
+        if least_days_task: # If there is a task
             least_days_label.set_text(f"Task with the least days left: {least_days_task['task']} ({least_days_task['frequency']} days)")
-        else:
+        else: # If there is no task
             least_days_label.set_text("You have no urgent tasks")
 
-    # Initial display of the task with the least amount of days left
-    least_days_label = ui.label()
-    update_least_days_label(rows)
+    least_days_label = ui.label() # initialize the label
+    update_task(rows) # update the label
 
 @ui.page('/reminders') # Route for reminders page
 def reminders():
