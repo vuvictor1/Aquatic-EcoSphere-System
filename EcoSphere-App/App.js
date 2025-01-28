52a8c538f744db02e66928bea1a7bcf5588b21e6
jmor1591 <bluejm01@gmail.com>
2025-01-27 17:03:09 -0800

    changes to app.js, set up firebase.js to

--- EcoSphere-App/App.js
+++ EcoSphere-App/App.js
@@ -3,11 +3,23 @@
 // Description: Mobile app for Aquatic EcoSphere web app.
 // Copyright (C) 2025 Victor V. Vu and Jordan Morris
 // License: GNU GPL v3 - See https://www.gnu.org/licenses/gpl-3.0.en.html
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { requestForToken, onMessageListener } from "./firebase";
 
 const App = () => { // App component
+  useEffect(() => {
+    requestForToken();
+
+    onMessageListener()
+      .then((payload) => {
+        console.log("Message received: ", payload);
+        // Show a notification or update the UI
+      })
+      .catch((err) => console.error("Error receiving message:", err));
+  }, []);
+
   return ( 
     <SafeAreaView style={styles.container}> 
       <WebView // WebView component to display the web app
@@ -17,7 +29,7 @@ const App = () => { // App component
         domStorageEnabled={true}
         startInLoadingState={true}
         allowsInlineMediaPlayback={true}
-        mediaPlaybackRequiresUserAction={false}
+        mediaPlaybackRequiresUser Action={false}
       />
     </SafeAreaView>
   );
